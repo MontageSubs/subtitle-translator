50e1e0d728499f0f83bb46000c943e770876b4a8
mtsubs <226338191+mtsubs@users.noreply.github.com>
2026-09-05 05:58:58 +0000

    update: worker v0.0.22-beta
    Source: 9df70380bd6b01d53c6742b893407b79d829779a

--- workers/api/src/index.ts
+++ workers/api/src/index.ts
@@ -6,7 +6,7 @@ import { handleTranslateJob } from "./handlers/translateJob";
 import { handleTurnstile } from "./handlers/turnstile";
 import { logHttp, logSecurity, logCron } from "./core/log";
 
-export const WORKER_VERSION = "0.0.21-beta";
+export const WORKER_VERSION = "0.0.22-beta";
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
